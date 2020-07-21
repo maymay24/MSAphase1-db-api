5549aeff00a76bbac308bd48501542a6f0c7417d
May <maywenchoy24@gmail.com>
2020-07-21 21:08:58 +1200

    Initial commit

---
+++ StudentSIMS/StudentSIMS/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentSIMS.Models
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int studentId { get; set; }
+        [Required, MaxLength(100)]
+        public string firstName { get; set; }
+        public string middleName { get; set; }
+        [Required]
+        public string lastName { get; set; }
+        public string emailAddress { get; set; }
+        public int phoneNumber { get; set; }
+        [Timestamp]
+        public DateTime timeCreated { get; set; }
+    }
+}
