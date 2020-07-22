d4e500ef426d03bc216e9fc739831d8aa3cc3d25
May <maywenchoy24@gmail.com>
2020-07-22 13:12:48 +1200

    Added Address table

---
+++ StudentSIMS/StudentSIMS/Data/AddressContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using StudentSIMS.Models;
+
+namespace StudentSIMS.Data
+{
+    public class AddressContext : DbContext
+    {
+        // an empty constructor
+        public AddressContext() { }
+
+        // base(options) calls the base class's constructor,
+        // in this case, our base class is DbContext
+        public AddressContext(DbContextOptions<AddressContext> options) : base(options) { }
+
+        // Use DbSet<Student> to query or read and 
+        // write information about an Address
+        public DbSet<Address> Address { get; set; }
+        public static System.Collections.Specialized.NameValueCollection AppSettings { get; }
+
+        // configure the database to be used by this context
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+           .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+           .AddJsonFile("appsettings.json")
+           .Build();
+
+            // schoolSIMSConnection is the name of the key that
+            // contains the has the connection string as the value
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("schoolSIMSConnection"));
+        }
+    }
+}
+
---
+++ StudentSIMS/StudentSIMS/Models/Address.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentSIMS.Models
+{
+    public class Address
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int addressID { get; set; }
+        [ForeignKey("Student")]
+        public int studentID { get; set; }
+        [Required, MaxLength(100)]
+        public string streetNumber { get; set; }
+        [Required, MaxLength(100)]
+        public string street { get; set; }
+        [Required, MaxLength(100)]
+        public string suburb { get; set; }
+        [Required, MaxLength(100)]
+        public string city { get; set; }
+        [Required]
+        public int postcode { get; set; }
+        [Required, MaxLength(100)]
+        public string country { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
